198a497af5b6126adadefa7e6f3af23677d7e15a
Kochari <koceri.agalarov@gmail.com>
2020-03-12 13:55:12 +0400

    Steps -m "project"

---
+++ src/main/java/service/Service.java
@@ -0,0 +1,16 @@
+package service;
+
+import DAOFull.BookingDAO;
+
+public class Service {
+
+    BookingDAO daoBooking;
+    public Service(BookingDAO daoBooking) {
+        this.daoBooking = daoBooking;
+    }
+
+    public void getallFlights() {
+
+    }
+
+}
