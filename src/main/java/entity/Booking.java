f8bf606968f08e8808acaf3827b5bba87b3401e0
Mariam Memmedzadeh <memmedzademeryem24@gmail.com>
2020-03-12 11:28:11 +0400

    Steps

--- src/main/java/entity/Booking.java
+++ src/main/java/entity/Booking.java
@@ -1,6 +1,21 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Booking {
-    String person_name;
+
+    private int bookingID;
+    List<Person> persons = new ArrayList<>();
+    Flight flight;
+    User user;
     AirWays destination;
+
+    public Booking(int bookingID, List<Person> persons, Flight flight, User user, AirWays destination) {
+        this.bookingID = bookingID;
+        this.persons = persons;
+        this.flight = flight;
+        this.user = user;
+        this.destination = destination;
+    }
 }
