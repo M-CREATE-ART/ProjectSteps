1d582bf0390d1f6f6832b5a9c76ee50522d4c27e
Kochari <koceri.agalarov@gmail.com>
2020-03-12 11:28:40 +0400

    Steps -m "project"

--- src/main/java/entity/Flight.java
+++ src/main/java/entity/Flight.java
@@ -1,5 +1,17 @@
 package entity;
 
-public class Flight {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class    Flight<formattedLocalDateTime>  {
+    int count_persons;
+    AirWays destination;
+
+public String FlightDate(){
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    LocalDateTime localDateTime = LocalDateTime.now();
+    String formattedLocalDateTime = localDateTime.format(dateTimeFormatter);
+    return  formattedLocalDateTime;
+}
 
 }
